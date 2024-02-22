92ee4b2149f7dff1aac5d47e96e8b15847b77635
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-02-22 17:51:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def main_dashboard():
 
     st.markdown(f"<h1 style='text-align: center;'>Search Query Analysis</h1>", unsafe_allow_html=True)
 
-    data = pd.read_csv("Search terms report.csv")
+    data = pd.read_csv("Search terms report.csv", skiprows=2)
 
     st.write(data)
 
