641a56f248665e4c1a7708d533849018f4d9d162
reporting-MR <140208843+reporting-MR@users.noreply.github.com>
2024-02-23 16:26:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def main_dashboard():
         'Probability': positive_probabilities
     })
 
-    st.write(results_df.sort("Probability"))
+    st.write(results_df.sort_values(by="Probability"))
 
 if __name__ == '__main__':
     password_protection()
